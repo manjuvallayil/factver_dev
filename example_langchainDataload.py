2c26625065bff7e19288c652b23c93f987e7b11b
manjuvallayil <manjuvallayil@gmail.com>
2024-12-20 19:36:34 +1300

    eval_update

--- example_langchainDataload.py
+++ example_langchainDataload.py
@@ -12,7 +12,7 @@ from langchain_community.document_loaders import HuggingFaceDatasetLoader
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings.sentence_transformer import SentenceTransformerEmbeddings
 import os
-#os.environ['HUGGINGFACE_API_TOKEN']="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"
+#os.environ['HUGGINGFACE_API_TOKEN']="---"
 HUGGINGFACE_API_TOKEN = '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'
 dataset_name = 'manjuvallayil/factver_master'
 page_content_column = "Evidence_text"
